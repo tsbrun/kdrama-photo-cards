c78019b98f6dcc7e6ac01e7722bcf43111bfcae9
TSephanya Brun <tsbrun@icloud.com>
2021-03-04 16:23:37 -0500

    Define #logged_in? method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
     def current_user
         @user = User.find_by(id: session[:user_id])
     end
+
+    def logged_in?
+        !current_user.nil?
+    end
 end
