106f6fb489233fb3e71dc3bc520559125dc7928c
TSephanya Brun <tsbrun@icloud.com>
2021-03-04 15:26:17 -0500

    Refactor sessions routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   resources :users, only: [:new, :create]
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/login'
-  get 'sessions/welcome'
+  get 'login', to: 'sessions/new'
+  post 'login', to: 'sessions/create'
+  get 'welcome', to: 'sessions/welcome'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "home#index"
 end
