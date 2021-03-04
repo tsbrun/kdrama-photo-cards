cb78915178df35432c5e9c60f99f5db3c8bde742
TSephanya Brun <tsbrun@icloud.com>
2021-03-04 15:25:12 -0500

    Refactor user resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
+  resources :users, only: [:new, :create]
   get 'sessions/new'
   get 'sessions/create'
   get 'sessions/login'
   get 'sessions/welcome'
-  get 'users/new'
-  get 'users/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "home#index"
 end
