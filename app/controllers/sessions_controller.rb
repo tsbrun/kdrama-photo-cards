cb09da1a007615362dff24f58457105ae259c63e
TSephanya Brun <tsbrun@icloud.com>
2021-03-05 12:16:12 -0500

    Google Auth action to login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,6 +17,22 @@ class SessionsController < ApplicationController
     redirect_to login_path
   end
 
+  def googleAuth 
+    # Get access tokens from the google server
+    access_token = request.env["omniauth.auth"]
+    @user = User.from_omniauth(access_token)
+    session[:user_id] = @user.id
+    #log_in(user) => presuming we have a log_in method? Maybe make one? For now, just do it manually.
+    # Access_token is used to authenticate request made from the rails app to the google server
+    # @user.google_token = access_token.credentials.token => only need to do this if using google api
+    # Refresh_token to request new access_token 
+    # Note: Refresh_token only sent once during the first request
+    refresh_token =  access_token.credentials.refresh_token
+    @user.google_refresh_token = refresh_token if refresh_token.present?
+    @user.save 
+    redirect_to welcome_path
+  end
+
   def welcome
   end
 end
