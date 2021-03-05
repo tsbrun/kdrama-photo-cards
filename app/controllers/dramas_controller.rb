d9701160d62cd222594e8a8e6d09f24bfdb29916
TSephanya Brun <tsbrun@icloud.com>
2021-03-05 15:59:06 -0500

    Restrict access to logged in users

--- app/controllers/dramas_controller.rb
+++ app/controllers/dramas_controller.rb
@@ -1,4 +1,15 @@
 class DramasController < ApplicationController
+  before_action :require_login 
+
   def index
+  end
+
+  private 
+
+  def require_login 
+    unless logged_in?
+      flash[:error] = "Please log in."
+      redirect_to login_path 
+    end
   end
 end
