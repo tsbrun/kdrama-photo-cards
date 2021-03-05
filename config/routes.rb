f1addc90b8215c2121287d9132f77d32d5847714
TSephanya Brun <tsbrun@icloud.com>
2021-03-04 19:08:58 -0500

    Draw logout route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   get 'welcome', to: 'sessions#welcome'
+  get 'logout', to: 'sessions#logout'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "home#index"
 end
