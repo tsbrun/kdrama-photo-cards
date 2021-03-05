352b6a67cb3b1f69dbedfc6c04a434ed004b40da
TSephanya Brun <tsbrun@icloud.com>
2021-03-04 19:38:00 -0500

    Render sign up form if invalid user params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,16 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.create(user_params)
+    @user = User.new(user_params)
+    if @user.valid?
+      @user.save
 
-    session[:user_id] = @user.id
+      session[:user_id] = @user.id
 
-    redirect_to welcome_path
+      redirect_to welcome_path
+    else
+      render :new
+    end
   end
 
   private 
