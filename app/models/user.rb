6f3b421bc1b2b4b4cd285e7ac967f3c56c1ee1cd
TSephanya Brun <tsbrun@icloud.com>
2021-03-05 12:15:34 -0500

    #from_omniauth class method

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,12 @@ class User < ApplicationRecord
     has_secure_password
     validates_presence_of :username, :password
     validates_uniqueness_of :username 
+
+    def self.from_omniauth(auth)
+        # Creates a new user only if it doesn't exist
+        where(email: auth.info.email).first_or_initialize do |user|
+            user.name = auth.info.name 
+            user.email = auth.info.email
+        end
+    end
 end
