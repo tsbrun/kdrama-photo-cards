605465afd030bb88649dcca30eb0c37f5dbae62e
TSephanya Brun <tsbrun@icloud.com>
2021-03-05 12:17:31 -0500

    Run google oauth migrations on user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_04_201812) do
+ActiveRecord::Schema.define(version: 2021_03_05_165930) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "uid"
+    t.string "provider"
+    t.string "name"
+    t.string "email"
+    t.string "google_token"
+    t.string "google_refresh_token"
   end
 
 end
