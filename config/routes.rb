0415bab0f551224535847edb897c857e7060ac70
TSephanya Brun <tsbrun@icloud.com>
2021-03-05 11:56:09 -0500

    Add routes for google oauth

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   get 'welcome', to: 'sessions#welcome'
   get 'logout', to: 'sessions#logout'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
   root "home#index"
+
+  # Routes for Google authentication
+  get ‘auth/:provider/callback’, to: ‘sessions#googleAuth’
+  get ‘auth/failure’, to: redirect(‘/’)
 end
