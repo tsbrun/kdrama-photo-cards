dcfacb959b6bdb395354e3aa8783da2eafc1288e
TSephanya Brun <tsbrun@icloud.com>
2021-03-05 13:38:41 -0500

    Set environment file as local_env.yml

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,12 @@ module KdramaPhotoCards
     # config.action_view.field_error_proc = Proc.new { |html_tag, instance| 
     #   html_tag
     # }
+
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
   end
 end
