0c996e9af8e615b24192164d311fac6e95b9c511
TSephanya Brun <tsbrun@icloud.com>
2021-03-04 19:37:21 -0500

    Validate uniqueness of username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
     has_secure_password
     validates_presence_of :username, :password
-    validates_uniqueness_of :username
+    validates_uniqueness_of :username 
 end
