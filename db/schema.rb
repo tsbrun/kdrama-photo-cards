5f4e7ea00827d5ab2cbb58043d82e0dc3b4744af
TSephanya Brun <tsbrun@icloud.com>
2021-03-05 15:54:23 -0500

    Run drama migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_05_165930) do
+ActiveRecord::Schema.define(version: 2021_03_05_205014) do
+
+  create_table "dramas", force: :cascade do |t|
+    t.string "title"
+    t.string "img_url"
+    t.string "wiki_url"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
