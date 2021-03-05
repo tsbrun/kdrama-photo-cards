ef36192d588978c52abb77b42f80720a1a12109d
TSephanya Brun <tsbrun@icloud.com>
2021-03-05 16:08:57 -0500

    Make #require_login a helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
     helper_method :current_user
     helper_method :logged_in?
+    helper_method :require_login
 
     def current_user
         @user = User.find_by(id: session[:user_id])
@@ -8,5 +9,12 @@ class ApplicationController < ActionController::Base
 
     def logged_in?
         !current_user.nil?
+    end
+
+    def require_login 
+        unless logged_in?
+          flash[:error] = "Please log in."
+          redirect_to login_path 
+        end
     end
 end
--- app/controllers/dramas_controller.rb
+++ app/controllers/dramas_controller.rb
@@ -3,13 +3,4 @@ class DramasController < ApplicationController
 
   def index
   end
-
-  private 
-
-  def require_login 
-    unless logged_in?
-      flash[:error] = "Please log in."
-      redirect_to login_path 
-    end
-  end
 end
