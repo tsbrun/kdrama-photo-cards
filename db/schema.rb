e0434b8f16790a04c17b27ac06cedb001ea0544f
TSephanya Brun <tsbrun@icloud.com>
2021-03-05 16:24:46 -0500

    Run Character migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_05_210209) do
+ActiveRecord::Schema.define(version: 2021_03_05_212417) do
 
   create_table "actors", force: :cascade do |t|
     t.string "name"
+    t.string "gender"
+    t.string "img_url"
+    t.string "wiki_url"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "characters", force: :cascade do |t|
+    t.string "role"
     t.string "gender"
     t.string "img_url"
     t.string "wiki_url"
