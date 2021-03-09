be638deb5d8c950317093e28027e2d61ee82e61e
TSephanya Brun <tsbrun@icloud.com>
2021-03-09 12:22:56 -0500

    Many-to-many relationship between Actor and Drama

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,4 @@
 class Actor < ApplicationRecord
     has_many :characters
+    has_many :dramas, through: :characters
 end
--- app/models/drama.rb
+++ app/models/drama.rb
@@ -1,3 +1,4 @@
 class Drama < ApplicationRecord
     has_many :characters
+    has_many :actors, through: :characters
 end
