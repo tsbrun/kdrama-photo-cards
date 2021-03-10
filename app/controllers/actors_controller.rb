9972a7eada3c4e66bad20279921e90cd82676197
TSephanya Brun <tsbrun@icloud.com>
2021-03-09 19:30:54 -0500

    Add new and create actions with logic

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -4,4 +4,18 @@ class ActorsController < ApplicationController
     def index 
         @actors = Actor.all
     end
+
+    def new 
+        @actor = Actor.new
+    end
+
+    def create 
+        @actor = Actor.new(actor_params)
+        if @actor.save 
+            flash[:success] = "You've successfully added a new actor."
+            redirect_to actors_path
+        else
+            render :new
+        end
+    end
 end
