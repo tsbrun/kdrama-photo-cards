63796ad013cbd921f955be10b1cacc73529641cf
TSephanya Brun <tsbrun@icloud.com>
2021-03-10 13:18:33 -0500

    Find characters associated with drama in nested route

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -2,6 +2,6 @@ class CharactersController < ApplicationController
     before_action :require_login
     
     def index 
-        @characters = Character.all
+        @characters = Drama.find_by(id: params[:drama_id]).characters
     end
 end
