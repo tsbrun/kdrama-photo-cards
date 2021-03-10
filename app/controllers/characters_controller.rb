bf12123ec78bb3c6b43c03ee736e5b9317e748b9
TSephanya Brun <tsbrun@icloud.com>
2021-03-10 13:22:04 -0500

    Comment for :get_drama helper to code later

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,5 +1,6 @@
 class CharactersController < ApplicationController
     before_action :require_login
+    # before_action :get_drama
     
     def index 
         @characters = Drama.find_by(id: params[:drama_id]).characters
